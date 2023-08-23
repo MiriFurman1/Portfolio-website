1dec386fb11c4256c2a92403eea349c3ee6144b9
Miri <mirifurman@gmail.com>
2023-08-23 13:05:38 +0300

    particles added

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,10 @@
-const Avatar = () => {
-  return <div>Avatar</div>;
+
+import Image from "next/image";
+
+ const Avatar = () => {
+  return <div className="hidden xl:flex xl:max-w-none">
+    <Image src={'/avatar-final.png'} width={737} height={678} alt='' className="translate-z-10 w-full h-full"/>
+  </div>;
 };
 
 export default Avatar;

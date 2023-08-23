dc2f27423760581901e6b095834f46e0561f3e3a
Miri <mirifurman@gmail.com>
2023-08-23 14:52:30 +0300

    header finished

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,11 @@
+import Link from "next/link";
+import {RiLinkedinBoxFill, RiGithubFill, RiMailFill} from 'react-icons/ri'
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className="flex  items-center gap-x-5 text-xl">
+    <Link href={'https://www.linkedin.com/in/miri-furman'} className="hover:text-accent transition-all duration-300"><RiLinkedinBoxFill/></Link>
+    <Link href={'https://github.com/MiriFurman1'} className="hover:text-accent transition-all duration-300"><RiGithubFill/></Link>
+    <Link href={'mailto:mirifurman@gmail.com'} className="hover:text-accent transition-all duration-300"><RiMailFill/></Link>
+    </div>;
 };
 
 export default Socials;

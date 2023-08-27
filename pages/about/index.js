46dfe3db13ca50ff1c7deca0d2ee42e06495059c
Miri <mirifurman@gmail.com>
2023-08-27 21:26:06 +0300

    about data changed

--- pages/about/index.js
+++ pages/about/index.js
@@ -25,59 +25,42 @@ const aboutData = [
           <FaReact />,
           <SiNextdotjs />,
           <SiFramer />,
-          <FaWordpress />,
+
         ],
-      },
-      {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
       },
     ],
   },
   {
-    title: 'awards',
-    info: [
-      {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
+
   },
   {
     title: 'experience',
     info: [
       {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        title: 'Fullstack Developer Intern',
+        stage: '2023',
       },
       {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
+        title: 'Chemical Analyst Hylabs Laboratories',
+        stage: '2020 - 2022',
       },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
+
     ],
   },
   {
-    title: 'credentials',
+    title: 'certificates',
     info: [
       {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
+        title: 'AI workshop Appleseeds ',
+        stage: '2013',
       },
       {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
+        title: 'Fullstack Bootcamp Appleseeeds',
+        stage: '2022-2023',
       },
       {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
+        title: 'B.Sc Chemical Engineering Ben Gurion University of The Negev',
+        stage: '2015-2020',
       },
     ],
   },

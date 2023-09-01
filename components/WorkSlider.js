41e011a9aaf730d91e7ac0a6f5c3ebdbf1cd4d08
Miri <mirifurman@gmail.com>
2023-09-01 12:02:52 +0300

    projects page finished

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -7,37 +7,42 @@ import 'swiper/css/grid';
 
 import { Grid, Pagination } from "swiper";
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { BsArrowRight } from 'react-icons/bs'
 const workSlides = [{
   title: 'Happily Ever After',
   path: '/HappilyEverAfter.png',
-  description:'Web app for wedding planning, including to-do lists, guest management, emails, and QR-based photo sharing.',
-  demoLink:'https://happily-ever-after.netlify.app/',
-  githubLink:'https://github.com/MiriFurman1/Happily-Ever-After'
+  description: 'Web app for wedding planning, including to-do lists, guest management, emails, and QR-based photo sharing.',
+  demoLink: 'https://happily-ever-after.netlify.app/',
+  githubLink: 'https://github.com/MiriFurman1/Happily-Ever-After',
+  technologies: ['React', 'NodeJs', 'Express', 'MongoDB']
 },
 {
   title: 'Happy Pet',
   path: '/HappyPet.png',
-  description:'Web project using the PetFinder API to simplify pet adoption by connecting organizations and individuals to adoptable pets.',
-  demoLink:'https://happy-pet-app.netlify.app/',
-  githubLink:'https://github.com/MiriFurman1/Happy-pet'
+  description: 'Web project using the PetFinder API to simplify pet adoption by connecting individuals to adoptable pets.',
+  demoLink: 'https://happy-pet-app.netlify.app/',
+  githubLink: 'https://github.com/MiriFurman1/Happy-pet',
+  technologies: ['React', 'Firebase']
 },
 {
   title: 'NextAd',
   path: '/NextAd.png',
-  description:'Dynamic landing page Creation for Small Businesses',
-  demoLink:'',
-  githubLink:''
+  description: 'Dynamic landing page Creation for Small Businesses',
+  demoLink: '',
+  githubLink: '',
+  technologies: ['NextJS', 'NodeJs', 'MySql']
 },
 {
   title: 'Wether Vision',
   path: '/WeatherVision1.png',
-  description:'Web app that showcases city weather via AccuWeather API with search, and favorites page',
-  demoLink:'https://weather-app-miri.netlify.app/',
-  githubLink:'https://github.com/MiriFurman1/Weather-App'
+  description: 'Web app that showcases city weather via AccuWeather API with search, and favorites page',
+  demoLink: 'https://weather-app-miri.netlify.app/',
+  githubLink: 'https://github.com/MiriFurman1/Weather-App',
+  technologies: ['React', 'MUI', 'Zustand']
 },]
- 
+
 
 
 
@@ -52,23 +57,37 @@ const WorkSlider = () => {
     grid={{
       rows: 1,
     }}
-    modules={[Grid,Pagination]}
+    modules={[Grid, Pagination]}
     className="h-[300px] "
   >
     {workSlides.map((slide, index) => {
-      return (<SwiperSlide  key={index} >
-        <div className=' gap-4 cursor pointer '>
-              <div key={index} className="relative rounded-lg overflow-hidden flex items-center justify-center group">
-                <div className='flex flex-col items-center justify-center relative overflow-hidden '>
-                  <Image src={slide.path} width={500} height={300} alt="" className='rounded-lg w-[500px]'/>
-                  {/* overlay gradient */}
-                  <div className='absolute inset-0 bg-gradient-to-r from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-60 transition-all duration-700'></div>
-                  <div className='flex flex-col items-center absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
-                    <div>{slide.title}</div>
-                    <div>{slide.description}</div>
-                  </div>
+      return (<SwiperSlide key={index} >
+        <div className=' gap-4 cursor pointer text-center text-white '>
+          <div key={index} className="relative rounded-lg overflow-hidden flex items-center justify-center group">
+            <div className='flex flex-col items-center justify-center relative overflow-hidden '>
+              <Image src={slide.path} width={500} height={300} alt="" className='rounded-lg w-[500px]' />
+              {/* overlay gradient */}
+              <div className='absolute inset-0 bg-gradient-to-r from-[#787373] via-[#e24eca] to-[#351496] opacity-0 group-hover:opacity-75 transition-all duration-700'></div>
+              <div className='flex flex-col w-[90%] items-center justify-center absolute bottom-0 translate-y-full group-hover:-translate-y-5  transition-all duration-300'>
+                <div className='text-xl'>{slide.title}</div>
+                <div className='hidden md:block'>{slide.description}</div>
+
+                <div className='flex text-sm'>
+                  <Link href={slide.demoLink} className='m-2 bg-white text-black p-2 rounded hover:bg-[#351496] hover:text-white transition-all duration-400'>Demo</Link>
+                  <Link href={slide.githubLink} className='m-2 bg-white text-black p-2 rounded hover:bg-[#351496] hover:text-white transition-all duration-400'>Github</Link>
                 </div>
+
+                <div className='flex flex-row flex-wrap text-sm'>
+                  {slide.technologies.map((tech, index) => {
+                    return <p key={index} className='m-1'>
+                      {tech}
+                    </p>
+                  })}
+                </div>
+
               </div>
+            </div>
+          </div>
         </div>
       </SwiperSlide>
       )
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import ParticlesContainer from '../components/ParticlesContainer'
 import ProjectsBtn from '../components/ProjectsBtn'
 import Avatar from '../components/Avatar'
@@ -13,12 +14,11 @@ const Home = () => {
         <motion.h1 className="h1" variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden"> Transforming Ideas <br /> Into <span className="text-accent"> Digital Reality</span></motion.h1>
         <motion.p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 text-base xl:text-lg" variants={fadeIn('down', 0.3)} initial="hidden" animate="show" exit="hidden">
           Welcome to my portfolio, where creativity knows no bounds and ideas take shape in the realm of possibilities. <br /> Each project is an exploration, a testament to dedication, revealing the limitless synergy of imagination and technology.<br /> Join me on this journey to touch the sky through artistry.</motion.p>
-        <div className=" xl:hidden relative md:flex sm:flex">
-          <ProjectsBtn />
-        </div>
-        <motion.div variants={fadeIn('down', 0.4)} initial="hidden" animate="show" exit="hidden" className="hidden xl:flex">
-          <ProjectsBtn />
-        </motion.div>
+
+
+<Link href="/projects">link!</Link>
+
+
       </div>
     </div>
 
